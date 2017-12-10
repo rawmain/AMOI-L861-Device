31d7f674662125d15217eb738507a97ca6c43949
rawmain <rawmain@gmail.com>
2017-12-10 03:49:19 +0100

    Kernel sources

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
+# file := $(INSTALLED_KERNEL_TARGET)
+# ALL_PREBUILT += $(file)
+# $(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+# 	$(transform-prebuilt-to-target)

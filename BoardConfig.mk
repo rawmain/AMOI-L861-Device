9618e983a2f6aec91a26efce3b2cb51810501010
RAW MAIN <rawmain@gmail.com>
2017-12-12 18:03:16 +0100

    Use prebuilt kernel
    Use prebuilt kernel (L861 sources - UberTC 4.9 - SELinux policy fix fo MM)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,15 +48,15 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board 1453346013
-# TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/L861_MM_Image.gz-dtb
 # PRODUCT_COPY_FILES += \
 #        $(TARGET_PREBUILT_KERNEL):kernel
 
 # Kernel Sources
-TARGET_KERNEL_SOURCE := kernel/amoi/l861
-TARGET_KERNEL_CONFIG := amoi_l861_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+# TARGET_KERNEL_SOURCE := kernel/amoi/l861
+# TARGET_KERNEL_CONFIG := amoi_l861_defconfig
+# TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+# BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 
 TARGET_KMODULES := true

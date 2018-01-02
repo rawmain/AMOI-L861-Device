f7a0c28e06816c88c545b7b5bba9f84134f2305a
RAW MAIN <rawmain@gmail.com>
2018-01-02 16:52:24 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board 1453346013
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/L861_MM_Image.gz-dtb
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 # PRODUCT_COPY_FILES += \
 #        $(TARGET_PREBUILT_KERNEL):kernel
 
